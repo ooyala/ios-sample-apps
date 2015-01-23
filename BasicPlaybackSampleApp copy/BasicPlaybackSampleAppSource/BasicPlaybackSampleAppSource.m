26983a72d09e365c22513d71b8201e9e05ecd929
Yi Gu <yigu@ooyala.com>
2015-01-23 10:46:49 -0800

    Add BasicPlaybackSampleApp to Sample App Repo for IOS, bug fixed for completeSampleApp

--- BasicPlaybackSampleApp copy/BasicPlaybackSampleAppSource/BasicPlaybackSampleAppSource.m
+++ BasicPlaybackSampleApp copy/BasicPlaybackSampleAppSource/BasicPlaybackSampleAppSource.m
@@ -6,7 +6,7 @@
 //  Copyright (c) 2014 Ooyala, Inc. All rights reserved.
 //
 
-#import "AdvancedPlaybackSampleAppSource.h"
+#import "BaiscPlaybackSampleAppSource.h"
 
 @implementation BasicPlaybackSampleAppSource
 
