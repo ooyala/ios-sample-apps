41f15323b0d341f7887b8322715f4cd03a31e9d4
Zhihui Chen <zchen@ooyala.com>
2015-02-18 17:03:16 -0800

    [PBA-1392] Placeholder for channelbrowser sample app

---
+++ ChannelBrowserSampleApp/ChannelBrowserSampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ChannelBrowserSampleApp
+//
+//  Created by Zhihui Chen on 2/18/15.
+//  Copyright (c) 2015 ooyala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ChannelBrowserSampleApp/ChannelBrowserSampleAppTests/ChannelBrowserSampleAppTests.m
@@ -0,0 +1,40 @@
+//
+//  ChannelBrowserSampleAppTests.m
+//  ChannelBrowserSampleAppTests
+//
+//  Created by Zhihui Chen on 2/18/15.
+//  Copyright (c) 2015 ooyala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <XCTest/XCTest.h>
+
+@interface ChannelBrowserSampleAppTests : XCTestCase
+
+@end
+
+@implementation ChannelBrowserSampleAppTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    XCTAssert(YES, @"Pass");
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
