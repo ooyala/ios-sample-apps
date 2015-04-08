d1fbebd7ea590e49353886fb2eb0f367701a8817
Matthew Chen <zzzzhihui@gmail.com>
2015-04-08 16:49:09 -0700

    [NielsenSampleApp] Port Nielsen sample app to iOS_sample_app

---
+++ AnalyticsPluginSampleApp/AnalyticsPluginSampleApp/Lists/NielsenPlaybackListViewcontroller.h
@@ -0,0 +1,12 @@
+/**
+ * @class      NielsenPlaybackListViewcontroller NielsenPlaybackListViewcontroller.h "IMAPlaybackListViewcontroller.h"
+ * @brief      A list of playback examples that demonstrate IMA Ad playback
+ * @date       12/12/14
+ * @copyright  Copyright (c) 2014 Ooyala, Inc. All rights reserved.
+ */
+
+#import <UIKit/UIKit.h>
+
+@interface NielsenPlaybackListViewcontroller : UITableViewController <UITableViewDelegate>
+
+@end
