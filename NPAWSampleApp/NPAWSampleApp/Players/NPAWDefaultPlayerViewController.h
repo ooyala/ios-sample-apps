f716323a0f5c60fb88d89cc99d20afa6b1aff7aa
Michael Len <michael.len@ooyala.com>
2015-05-26 17:52:30 -0700

    [PBA-1548] Add NPAW OptionalMetadata Player

--- NPAWSampleApp/NPAWSampleApp/Players/NPAWDefaultPlayerViewController.h
+++ NPAWSampleApp/NPAWSampleApp/Players/NPAWDefaultPlayerViewController.h
@@ -1,7 +1,7 @@
 /**
- * @class      BasicSimplePlayerViewController BasicSimplePlayerViewController.h "BasicSimplePlayerViewController.h"
- * @brief      A Player that can be used to simply load an embed code and play it
- * @details    SimplePlayerViewController in Ooyala Sample Apps
+ * @class      NPAWDefaultPlayerViewController NPAWDefaultPlayerViewController.h "NPAWDefaultPlayerViewController.h"
+ * @brief      A Player that demonstrates basic NPAW Youbora integration
+ * @details    NPAWDefaultPlayerViewController in Ooyala Sample Apps
  * @date       01/12/15
  * @copyright  Copyright (c) 2015 Ooyala, Inc. All rights reserved.
  */
--- NPAWSampleApp/NPAWSampleApp/Players/NPAWOptionalMetadataPlayerViewController.m
+++ NPAWSampleApp/NPAWSampleApp/Players/NPAWOptionalMetadataPlayerViewController.m
@@ -1,32 +1,31 @@
 /**
- * @class      NPAWDefaultPlayerViewController NPAWDefaultPlayerViewController.m "NPAWDefaultPlayerViewController.m"
- * @brief      A Player that demonstrates basic NPAW Youbora integration
- * @details    NPAWDefaultPlayerViewController in Ooyala Sample Apps
+ * @class      NPAWOptionalMetadataPlayerViewController NPAWOptionalMetadataPlayerViewController.m "NPAWOptionalMetadataPlayerViewController.m"
+ * @brief      A Player that demonstrates NPAW Youbora integration with optional metadata
+ * @details    NPAWOptionalMetadataPlayerViewController in Ooyala Sample Apps
  * @copyright  Copyright (c) 2015 Ooyala, Inc. All rights reserved.
  */
 
+#import "NPAWOptionalMetadataPlayerViewController.h"
 
-#import "NPAWDefaultPlayerViewController.h"
 #import <YouboraMedia/Youbora.h>
 #import <OoyalaSDK/OOOoyalaPlayerViewController.h>
 #import <OoyalaSDK/OOOoyalaPlayer.h>
 #import <OoyalaSDK/OOPlayerDomain.h>
 
-@interface NPAWDefaultPlayerViewController ()
+@interface NPAWOptionalMetadataPlayerViewController ()
 @property (strong, nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 
 @property NSString *embedCode;
 @property NSString *nib;
 @property NSString *pcode;
 @property NSString *playerDomain;
-@property (nonatomic, strong) Youbora *youbora;
 @property NSString *npawSystemId;
 @property NSString *npawUserId;
+@property (nonatomic, strong) Youbora *youbora;
 
 @end
 
-
-@implementation NPAWDefaultPlayerViewController
+@implementation NPAWOptionalMetadataPlayerViewController
 
 - (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption {
   self = [super initWithPlayerSelectionOption: playerSelectionOption];
@@ -35,7 +34,7 @@
   self.playerDomain = @"http://www.ooyala.com";
   self.npawSystemId = @"ooyalaqa";
   self.npawUserId = @"pkArmh";
-  
+
   if (self.playerSelectionOption) {
     self.embedCode = self.playerSelectionOption.embedCode;
     self.title = self.playerSelectionOption.title;
@@ -54,33 +53,51 @@
   // Create Ooyala ViewController
   OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
   self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
-  
+
   [[NSNotificationCenter defaultCenter] addObserver: self
                                            selector:@selector(notificationHandler:)
                                                name:nil
                                              object:self.ooyalaPlayerViewController.player];
 
+
+  // Get the custom metadata
+  NSDictionary *customMetadata = [self generateNpawCustomMetadata];
+
   // initialize youbora plugin
-  self.youbora = [[Youbora alloc] initWithSystemId:self.npawSystemId userID:self.npawUserId playerInstance:player options:nil];
+  self.youbora = [[Youbora alloc] initWithSystemId:self.npawSystemId userID:self.npawUserId playerInstance:player options:customMetadata];
 
   // Attach it to current view
   [self addChildViewController:self.ooyalaPlayerViewController];
   [self.playerView addSubview:self.ooyalaPlayerViewController.view];
   [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
-  
+
   // Load the video
   [self.ooyalaPlayerViewController.player setEmbedCode:self.embedCode];
   [self.ooyalaPlayerViewController.player play];
+}
 
+/*
+ * Youbora optional parameters
+ */
+- (NSDictionary*)generateNpawCustomMetadata
+{
+  NSDictionary *optionsCustomData = @{ @"customData":
+                                         @{@"filename": @"Example Custom File Name",
+                                           @"content_metadata":
+                                             @{@"title": @"Custom Overridden Title", @"genre": @"Custom Genre"},
+                                           @"device":
+                                             @{@"manufacturer": @"Custom Device Manufacturer"}}
+                                    };
+  return optionsCustomData;
 }
 
 - (void) notificationHandler:(NSNotification*) notification {
-  
+
   // Ignore TimeChangedNotificiations for shorter logs
   if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
     return;
   }
-  
+
   NSLog(@"Notification Received: %@. state: %@. playhead: %f",
         [notification name],
         [OOOoyalaPlayer playerStateToString:[self.ooyalaPlayerViewController.player state]],
