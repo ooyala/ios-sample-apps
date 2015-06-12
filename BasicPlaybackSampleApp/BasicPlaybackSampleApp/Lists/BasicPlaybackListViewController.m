c59ae4edb38dae26e8d99180eb2c9c376b4a9989
Michael Len <michael.len@ooyala.com>
2015-06-12 14:52:15 -0700

    Update BasicPlaybackSampleApp list view code to match other sample apps

--- BasicPlaybackSampleApp/BasicPlaybackSampleApp/Lists/BasicPlaybackListViewController.m
+++ BasicPlaybackSampleApp/BasicPlaybackSampleApp/Lists/BasicPlaybackListViewController.m
@@ -26,49 +26,25 @@
 }
 
 - (void)addAllBasicPlayerSelectionOptions {
-  for(long i = [self.optionList count] - 1; i >= 0; i--) {
-    [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:[self.optionList objectAtIndex:i] embedCode:[self.optionEmbedCodes objectAtIndex:i] viewController: [BasicSimplePlayerViewController class]]];
-  }
-}
-
-- (void) initTitlesAndEmbedCodes {
-  self.optionList = [[NSMutableArray alloc] initWithObjects:
-                     @"HLS Video",
-                     @"MP4 Video",
-                     @"VOD with CCs",
-                     @"4:3 Aspect Ratio",
-                     @"VAST Ad Pre-roll",
-                     @"VAST Ad Mid-roll",
-                     @"VAST Ad Post-roll",
-                     @"VAST Ad Wrapper",
-                     @"Ooyala Ad Pre-roll",
-                     @"Ooyala Ad Mid-roll",
-                     @"Ooyala Ad Post-roll",
-                     @"Multi Ad combination", nil];
-  
-  self.optionEmbedCodes = [[NSMutableArray alloc] initWithObjects:
-                           @"Y1ZHB1ZDqfhCPjYYRbCEOz0GR8IsVRm1",
-                           @"h4aHB1ZDqV7hbmLEv4xSOx3FdUUuephx",
-                           @"92cWp0ZDpDm4Q8rzHfVK6q9m6OtFP-ww",
-                           @"FwaXZjcjrkydIftLal2cq9ymQMuvjvD8",
-                           @"Zlcmp0ZDrpHlAFWFsOBsgEXFepeSXY4c", // VAST Preroll
-                           @"pncmp0ZDp7OKlwTPJlMZzrI59j8Imefa", // VAST Midroll
-                           @"Zpcmp0ZDpaB-90xK8MIV9QF973r1ZdUf", // VAST Postroll
-                           @"pqaWp0ZDqo17Z-Dn_5YiVhjcbQYs5lhq", // VAST Wrapper
-                           @"M4cmp0ZDpYdy8kiL4UD910Rw_DWwaSnU", // OOYALA Preroll
-                           @"xhcmp0ZDpnDB2-hXvH7TsYVQKEk_89di", // OOYALA Midroll
-                           @"Rjcmp0ZDr5yFbZPEfLZKUveR_2JzZjMO", // OOYALA Postroll
-                           @"Ftcmp0ZDoz8tALmhPcN2vMzCdg7YU9lc", // Multi Ad combination
-                           nil];
-  
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"HLS Video" embedCode:@"Y1ZHB1ZDqfhCPjYYRbCEOz0GR8IsVRm1" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"MP4 Video" embedCode:@"h4aHB1ZDqV7hbmLEv4xSOx3FdUUuephx" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"VOD with CCs" embedCode:@"92cWp0ZDpDm4Q8rzHfVK6q9m6OtFP-ww" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"4:3 Aspect Ratio" embedCode:@"FwaXZjcjrkydIftLal2cq9ymQMuvjvD8" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"VAST Ad Pre-roll" embedCode:@"Zlcmp0ZDrpHlAFWFsOBsgEXFepeSXY4c" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"VAST Ad Mid-roll" embedCode:@"pncmp0ZDp7OKlwTPJlMZzrI59j8Imefa" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"VAST Ad Post-roll" embedCode:@"Zpcmp0ZDpaB-90xK8MIV9QF973r1ZdUf" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"VAST Ad Wrapper" embedCode:@"pqaWp0ZDqo17Z-Dn_5YiVhjcbQYs5lhq" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"Ooyala Ad Pre-roll" embedCode:@"M4cmp0ZDpYdy8kiL4UD910Rw_DWwaSnU" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"Ooyala Ad Mid-roll" embedCode:@"xhcmp0ZDpnDB2-hXvH7TsYVQKEk_89di" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"Ooyala Ad Post-roll" embedCode:@"Rjcmp0ZDr5yFbZPEfLZKUveR_2JzZjMO" viewController: [BasicSimplePlayerViewController class]]];
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"Multi Ad combination" embedCode:@"Ftcmp0ZDoz8tALmhPcN2vMzCdg7YU9lc" viewController: [BasicSimplePlayerViewController class]]];
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   self.navigationController.navigationBar.translucent = NO;
   [self.tableView registerNib:[UINib nibWithNibName:@"TableCell" bundle:nil]forCellReuseIdentifier:@"TableCell"];
-  
-  [self initTitlesAndEmbedCodes];
+
   [self addAllBasicPlayerSelectionOptions];
 }
 
@@ -76,7 +52,7 @@
   if (!self.options) {
     self.options = [[NSMutableArray alloc] init];
   }
-  [self.options insertObject:selectionObject atIndex:0];
+  [self.options addObject:selectionObject];
   NSIndexPath *indexPath = [NSIndexPath indexPathForRow:0 inSection:0];
   [self.tableView insertRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationAutomatic];
 }
