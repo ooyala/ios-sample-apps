54fc98ef917def5b71de96df90ef0ea80b25d59b
Michael Len <michael.len@ooyala.com>
2015-09-30 13:52:16 -0700

    Update Google IMA libraries to match Ooyala SDK built version (3.0.b16)

---
+++ vendor/GoogleIMA/googleads-ima-ios-1.3/Classes/IMASampleAppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  IMASampleAppDelegate.h
+//  SampleAppV3
+//
+//  Copyright (c) 2013 Google Inc. All rights reserved.
+
+#import <UIKit/UIKit.h>
+
+@interface IMASampleAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property(strong, nonatomic) UIWindow *window;
+
+@end
