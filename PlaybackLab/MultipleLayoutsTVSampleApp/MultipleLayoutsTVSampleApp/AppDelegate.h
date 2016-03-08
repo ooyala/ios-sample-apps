167d86ea7bb2eb84b935021e2edd4c9cafd56772
Eric Vargas <evargas@ooyala.com>
2016-03-07 18:20:15 -0600

    [TVOS] Add tvos sample app

---
+++ PlaybackLab/MultipleLayoutsTVSampleApp/MultipleLayoutsTVSampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MultipleLayoutsTVSampleApp
+//
+//  Created by Eric Vargas on 3/7/16.
+//  Copyright © 2016 Ooyala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlaybackLab/MultipleLayoutsTVSampleApp/MultipleLayoutsTVSampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MultipleLayoutsTVSampleApp
+//
+//  Created by Eric Vargas on 3/7/16.
+//  Copyright © 2016 Ooyala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
