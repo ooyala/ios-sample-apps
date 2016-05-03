22af0bd2e9f9b8df93fe8fc8543c0d4b353855f0
Jon Slenk <jonslenk@ooyala.com>
2016-05-03 11:17:50 -0700

    [PBA-2933] demo the profiling.

---
+++ AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/PerformanceProfilingPlayerViewController.m
@@ -0,0 +1,82 @@
+/**
+ * @copyright  Copyright (c) 2016 Ooyala, Inc. All rights reserved.
+ */
+
+
+#import "PerformanceProfilingPlayerViewController.h"
+
+#import <OoyalaSDK/OOOoyalaPlayerViewController.h>
+#import <OoyalaSDK/OOOoyalaPlayer.h>
+#import <OoyalaSDK/OOPlayerDomain.h>
+#import <OoyalaSDK/OOPerformanceMonitorBuilder.h>
+
+@interface PerformanceProfilingPlayerViewController ()
+@property (nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
+@property (nonatomic) OOPerformanceMonitor *performanceMonitor;
+@property (nonatomic) NSString *embedCode;
+@property (nonatomic) NSString *nib;
+@property (nonatomic) NSString *pcode;
+@property (nonatomic) NSString *playerDomain;
+@end
+
+@implementation PerformanceProfilingPlayerViewController
+
+- (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption {
+  self = [super initWithPlayerSelectionOption: playerSelectionOption];
+  self.nib = @"PlayerSimple";
+  self.pcode =@"Vpd3E6BNabnn09G72IWye5O2RzN1";
+  self.playerDomain = @"http://www.ooyala.com";
+
+  if (self.playerSelectionOption) {
+    self.embedCode = self.playerSelectionOption.embedCode;
+    self.title = self.playerSelectionOption.title;
+  }
+  return self;
+}
+
+- (void)loadView {
+  [super loadView];
+  [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
+}
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Create Ooyala ViewController
+  // Create Ooyala ViewController
+  OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
+  self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
+  self.performanceMonitor = [OOPerformanceMonitorBuilder getStandardAdsMonitor];
+  
+  [[NSNotificationCenter defaultCenter] addObserver: self
+                                           selector:@selector(notificationHandler:)
+                                               name:nil
+                                             object:_ooyalaPlayerViewController.player];
+
+  // Attach it to current view
+  [self addChildViewController:_ooyalaPlayerViewController];
+  [self.playerView addSubview:_ooyalaPlayerViewController.view];
+  [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
+
+  // Load the video
+  [_ooyalaPlayerViewController.player setEmbedCode:self.embedCode];
+  [_ooyalaPlayerViewController.player play];
+}
+
+-(void) viewDidDisappear:(BOOL)animated {
+  NSLog( @"%@", [[self.performanceMonitor buildStatisticsSnapshot] generateReport]);
+}
+
+- (void) notificationHandler:(NSNotification*) notification {
+
+  // Ignore TimeChangedNotificiations for shorter logs
+  if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
+    return;
+  }
+
+  NSLog(@"Notification Received: %@. state: %@. playhead: %f",
+        [notification name],
+        [OOOoyalaPlayer playerStateToString:[self.ooyalaPlayerViewController.player state]],
+        [self.ooyalaPlayerViewController.player playheadTime]);
+}
+
+@end
