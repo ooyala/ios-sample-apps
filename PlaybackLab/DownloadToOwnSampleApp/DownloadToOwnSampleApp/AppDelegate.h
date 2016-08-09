570d67119866496ce2289f79945014fc35d5b1ef
Eric Vargas <evargas@ooyala.com>
2016-08-09 11:06:13 -0700

    [DTO] Created sample app

---
+++ PlaybackLab/DownloadToOwnSampleApp/DownloadToOwnSampleApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DownloadToOwnSampleApp
+//
+//  Created by Eric Vargas on 8/8/16.
+//  Copyright © 2016 Eric Vargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PlaybackLab/DownloadToOwnSampleApp/DownloadToOwnSampleApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DownloadToOwnSampleApp
+//
+//  Created by Eric Vargas on 8/8/16.
+//  Copyright © 2016 Eric Vargas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
