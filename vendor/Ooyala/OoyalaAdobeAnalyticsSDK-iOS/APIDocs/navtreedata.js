f6e4fde62b19bdc9ead0e61948990b1893788b7e
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2016-08-11 20:46:54 -0700

    [PBA-4380] Update to SDK Version v4.17.0_RC2

---
+++ vendor/Ooyala/OoyalaAdobeAnalyticsSDK-iOS/APIDocs/navtreedata.js
@@ -0,0 +1,24 @@
+var NAVTREE =
+[
+  [ "Ooyala-Adobe-Analytics-iOS-SDK", "index.html", [
+    [ "Classes", "annotated.html", [
+      [ "Class List", "annotated.html", "annotated_dup" ],
+      [ "Class Index", "classes.html", null ],
+      [ "Class Members", "functions.html", [
+        [ "All", "functions.html", null ],
+        [ "Functions", "functions_func.html", null ]
+      ] ]
+    ] ],
+    [ "Files", null, [
+      [ "File List", "files.html", "files" ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"_o_o_adobe_analytics_manager_8h_source.html"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
