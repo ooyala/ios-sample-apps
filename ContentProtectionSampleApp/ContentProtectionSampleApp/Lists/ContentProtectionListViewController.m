400e604d0817b1ac67cbd53bde19874be085ba6a
Michael Len <michael.len@ooyala.com>
2016-08-24 13:54:07 -0700

    Re-add Adobe Pass test case in ContentProtectionSampleApp

--- ContentProtectionSampleApp/ContentProtectionSampleApp/Lists/ContentProtectionListViewController.m
+++ ContentProtectionSampleApp/ContentProtectionSampleApp/Lists/ContentProtectionListViewController.m
@@ -32,6 +32,12 @@
   [super viewDidLoad];
   self.navigationController.navigationBar.translucent = NO;
   [self.tableView registerNib:[UINib nibWithNibName:@"TableCell" bundle:nil]forCellReuseIdentifier:@"TableCell"];
+
+  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"Adobe Pass"
+                                                            embedCode:@"VybW5lODrJ0uM9FBo7XTT6TNjTJfr_7G"
+                                                                pcode:@"B3MDExOuTldXc1CiXbzAauYN7Iui"
+                                                               domain:@"http://www.ooyala.com"
+                                                       viewController: [AdobePassPlayerViewController class]]];
   [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:@"Device Management (Unconfigured)"
                                                             embedCode:@""
                                                                 pcode:@"c0cTkxOqALQviQIGAHWY5hP0q9gU"
