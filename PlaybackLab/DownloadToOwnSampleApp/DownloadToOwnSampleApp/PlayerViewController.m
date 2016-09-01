416dba872469fcd6ac4a7693824a1c0e5bdacfdf
Eric Vargas <evargas@ooyala.com>
2016-09-01 11:37:33 -0700

    [PBA-4439] Listen to Download changes in player activity

--- PlaybackLab/DownloadToOwnSampleApp/DownloadToOwnSampleApp/PlayerViewController.m
+++ PlaybackLab/DownloadToOwnSampleApp/DownloadToOwnSampleApp/PlayerViewController.m
@@ -8,12 +8,16 @@
 
 #import "PlayerViewController.h"
 #import "PlayerSelectionOption.h"
+#import "AssetPersistenceManager.h"
 
 #import <OoyalaSDK/OoyalaSDK.h>
 
 @interface PlayerViewController ()
 
 @property (weak, nonatomic) IBOutlet UIView *playerView;
+@property (weak, nonatomic) IBOutlet UILabel *stateLabel;
+@property (weak, nonatomic) IBOutlet UIButton *playOfflineButton;
+
 @property (nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 
 @end
@@ -33,17 +37,58 @@
   [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
   [self.ooyalaPlayerViewController didMoveToParentViewController:self];
   
-  NSURL *location = [[NSUserDefaults standardUserDefaults] URLForKey:self.option.embedCode];
-  if (location) {
-    // offline mode
-    NSLog(@"Playing an offline video");
-    OOOfflineVideo *video = [[OOOfflineVideo alloc] initWithFileLocation:location];
-    [self.ooyalaPlayerViewController.player setUnbundledVideo:video];
-  } else {
-    // online mode
-    NSLog(@"Playing an online video");
-    [self.ooyalaPlayerViewController.player setEmbedCode:self.option.embedCode];
+  AssetPersistenceState state = [[AssetPersistenceManager sharedManager] downloadStateForEmbedCode:self.option.embedCode];
+  [self updateUIUsingState:@(state)];
+  
+  
+}
+
+- (void)viewWillAppear:(BOOL)animated {
+  [super viewWillAppear:animated];
+  [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(handleAssetStateChanged:) name:AssetPersistenceStateChangedNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+  [super viewWillDisappear:animated];
+  [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)updateUIUsingState:(NSNumber *)state {
+  switch ([state intValue]) {
+    case AssetNotDownloaded:
+      self.stateLabel.text = @"State: Not Downloaded";
+      self.playOfflineButton.enabled = NO;
+      break;
+    case AssetDownloading:
+      self.stateLabel.text = @"State: Downloading";
+      self.playOfflineButton.enabled = NO;
+      break;
+    case AssetDownloaded:
+      self.stateLabel.text = @"State: Downloaded";
+      self.playOfflineButton.enabled = YES;
+      break;
   }
+}
+
+- (void)handleAssetStateChanged:(NSNotification *)notification {
+  NSString *embedCode = notification.userInfo[AssetNameKey];
+  NSNumber *state = notification.userInfo[AssetStateKey];
+  
+  if ([embedCode isEqualToString:self.option.embedCode]) {
+    [self updateUIUsingState:state];
+  }
+}
+
+- (IBAction)playOnline {
+  NSLog(@"Playing an online video");
+  [self.ooyalaPlayerViewController.player setEmbedCode:self.option.embedCode];
+}
+
+- (IBAction)playOffline {
+  NSLog(@"Playing an offline video");
+  NSURL *location = [[AssetPersistenceManager sharedManager] downloadLocationForEmbedCode:self.option.embedCode];
+  OOOfflineVideo *video = [[OOOfflineVideo alloc] initWithFileLocation:location];
+  [self.ooyalaPlayerViewController.player setUnbundledVideo:video];
 }
 
 @end
