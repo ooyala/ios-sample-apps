0d8b39e134bb872ea282d6da6e89815e2e74f0b5
Michael Len <michael.len@ooyala.com>
2016-11-21 15:25:17 -0800

    [PBA-4570] Add IQConfiguration to OptionsSampleApp

---
+++ OptionsSampleApp/OptionsSampleApp/Players/IQConfigurationPlayerViewController.h
@@ -0,0 +1,15 @@
+/**
+ * @class      IQConfigurationPlayerViewController IQConfigurationPlayerViewController.h "IQConfigurationPlayerViewController.h"
+ * @brief      A Player that can be used to demonstrate IQ Configuration
+ * @details    A Player that can be used to demonstrate IQ Configuration
+ * @copyright  Copyright (c) 2015 Ooyala, Inc. All rights reserved.
+ */
+
+
+#import <UIKit/UIKit.h>
+#import "SampleAppPlayerViewController.h"
+
+@interface IQConfigurationPlayerViewController : SampleAppPlayerViewController
+
+@end
+
