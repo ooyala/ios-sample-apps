d5b3e2fe270eede3224afeee41be7bb387eced78
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2017-01-23 18:37:27 -0800

    [PBA-5303] Update to SDK Version v4.22.0_RC3

--- vendor/Ooyala/OoyalaSDK-tvOS/OoyalaSDK.framework/Versions/A/Headers/OOSecureURLGenerator.h
+++ vendor/Ooyala/OoyalaSDK-tvOS/OoyalaSDK.framework/Versions/A/Headers/OOSecureURLGenerator.h
@@ -9,7 +9,7 @@
  * Generate the secure URL
  * @par
  * This method should use one of the following security method to create a complete NSURL:
- * @li Create a signature from the parameters (including API Key and Domain, which are not guarenteed to be in params) and a secret
+ * @li Create a signature from the parameters (including API Key and Domain, which are not guaranteed to be in params) and a secret
  * @param[in] host the hostname for the URL
  * @param[in] uri the URI for the URL
  * @param[in] params the URI params for the URL (not including any security params that the security method would use)
