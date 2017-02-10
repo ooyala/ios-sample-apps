395db4eb55590926a24758536775f2ad97d62a39
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2017-02-09 16:13:55 -0800

    Update Apps for dev build: SDK SHA  695c7d51 - Skin SHA 073111566

--- vendor/Ooyala/OoyalaSDK-iOS/OoyalaSDK.framework/Headers/OOOoyalaError.h
+++ vendor/Ooyala/OoyalaSDK-iOS/OoyalaSDK.framework/Headers/OOOoyalaError.h
@@ -13,30 +13,36 @@
  * Error Codes
  */
 typedef enum {
-  OOOoyalaErrorCodeAuthorizationFailed, /**< Authorization Failed */
-  OOOoyalaErrorCodeAuthorizationInvalid, /**< Authorization Response invalid */
-  OOOoyalaErrorCodeHeartbeatFailed, /**< Heartbeat failed */
-  OOOoyalaErrorCodeContentTreeInvalid, /**< Content Tree Response invalid */
-  OOOoyalaErrorCodeAuthorizationSignatureInvalid, /**< The signature of the Authorization Response is invalid */
-  OOOoyalaErrorCodeContentTreeNextFailed, /**< Content Tree Next failed */
-  OOOoyalaErrorCodePlaybackFailed, /**< AVPlayer Failed */
-  OOOoyalaErrorCodeAssetNotEncodedForIOS, /**< The asset is not encoded for iOS */
-  OOOoyalaErrorCodeInternalIOS, /**< An Internal iOS Error. Check the error property. */
-  OOOoyalaErrorCodeMetadataInvalid, /**< Metadata Response invalid */
-  OOOoyalaErrorCodeDeviceInvalidAuthToken, /**< During DRM Rights Acquisition, the rights server reported an invalid token */
-  OOOoyalaErrorCodeDeviceLimitReached, /**<  During DRM Rights Acquisition, the server reported that the device limit has been reached*/
-  OOOoyalaErrorCodeDeviceBindingFailed, /**<  During DRM Rights Acquisition, the server reported that device binding failed */
-  OOOoyalaErrorCodeDeviceIdTooLong, /**<  During DRM Rights Acquisition, the server reported the device id was too long */
-  OOOoyalaErrorCodeDeviceGenericDrmError, /**< There was some unknown error during the DRM workflow */
-  OOOoyalaErrorCodeDrmDownloadFailedError, /**< Failed to download a required file during the DRM workflow */
-  OOOoyalaErrorCodeDrmPersonalizationFailedError, /**< Failed to complete device personalization during the DRM workflow */
-  OOOoyalaErrorCodeDrmAcquireRightsFailedError, /**< Failed to get rights for asset during the DRM workflow */
-  OOOoyalaErrorCodeDiscoveryInvalidParameter, /**< The expected discovery parameters are not provided */
-  OOOoyalaErrorCodeDiscoveryNetworkError, /**< The discovery response is not received due to network errors */
-  OOOoyalaErrorCodeDiscoveryFailedResponse, /**< The discovery reponse is received and an error occured on server side */
-  OOOoyalaErrorCodeNoAvailableStreams, /**< There were no streams available to play */
-  OOOoyalaErrorCodePcodeMismatch,
-  OOOoyalaErrorCodeDownloadFailed, /**< Download Failed for any reason */
+  OOOoyalaErrorCodeAuthorizationFailed = 0, /**< Authorization Failed */
+  OOOoyalaErrorCodeAuthorizationInvalid = 1, /**< Authorization Response invalid */
+  OOOoyalaErrorCodeHeartbeatFailed = 2, /**< Heartbeat failed */
+  OOOoyalaErrorCodeContentTreeInvalid = 3, /**< Content Tree Response invalid */
+  OOOoyalaErrorCodeAuthorizationSignatureInvalid = 4, /**< The signature of the Authorization Response is invalid */
+  OOOoyalaErrorCodeContentTreeNextFailed = 5, /**< Content Tree Next failed */
+  OOOoyalaErrorCodePlaybackFailed = 6, /**< AVPlayer Failed */
+  OOOoyalaErrorCodeAssetNotEncodedForIOS = 7, /**< The asset is not encoded for iOS */
+  OOOoyalaErrorCodeInternalIOS = 8, /**< An Internal iOS Error. Check the error property. */
+  OOOoyalaErrorCodeMetadataInvalid = 9, /**< Metadata Response invalid */
+  OOOoyalaErrorCodeDeviceInvalidAuthToken = 10, /**< During DRM Rights Acquisition, the rights server reported an invalid token */
+  OOOoyalaErrorCodeDeviceLimitReached = 11, /**<  During DRM Rights Acquisition, the server reported that the device limit has been reached*/
+  OOOoyalaErrorCodeDeviceBindingFailed = 12, /**<  During DRM Rights Acquisition, the server reported that device binding failed */
+  OOOoyalaErrorCodeDeviceIdTooLong = 13, /**<  During DRM Rights Acquisition, the server reported the device id was too long */
+  OOOoyalaErrorCodeDeviceGenericDrmError = 14, /**< There was some unknown error during the DRM workflow */
+  OOOoyalaErrorCodeDrmDownloadFailedError = 15, /**< Failed to download a required file during the DRM workflow */
+  OOOoyalaErrorCodeDrmPersonalizationFailedError = 16, /**< Failed to complete device personalization during the DRM workflow */
+  OOOoyalaErrorCodeDrmAcquireRightsFailedError = 17, /**< Failed to get rights for asset during the DRM workflow */
+  OOOoyalaErrorCodeDiscoveryInvalidParameter = 18, /**< The expected discovery parameters are not provided */
+  OOOoyalaErrorCodeDiscoveryNetworkError = 19, /**< The discovery response is not received due to network errors */
+  OOOoyalaErrorCodeDiscoveryFailedResponse = 20, /**< The discovery reponse is received and an error occured on server side */
+  OOOoyalaErrorCodeNoAvailableStreams = 21, /**< There were no streams available to play */
+  OOOoyalaErrorCodePcodeMismatch = 22, /**< Provided PCode does not match embed code owner */
+  OOOoyalaErrorCodeDownloadFailed = 23, /**< Download Failed for any reason */
+  OOOoyalaErrorCodeDeviceConcurrentStreams = 24, /**< Concurrent streams limit reached */
+  OOOoyalaErrorCodeAdvertistingIdFailure = 25, /**< Failed to obtain advertising Id */
+  OOOoyalaErrorCodeDiscoveryGetFailure = 26, /**< Failed to get discovery results */
+  OOOoyalaErrorCodeDiscoveryPostFailure = 27, /**< Failed to post discovery pins */
+  OOOoyalaErrorCodeUnknownError = 28, /**< Unknown error */
+  
 } OOOoyalaErrorCode;
 
 
