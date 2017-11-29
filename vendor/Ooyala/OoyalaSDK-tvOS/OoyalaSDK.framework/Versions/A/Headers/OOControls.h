29163a6e8e1e29450a3cb7b3562068e1015113f4
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2017-11-28 18:23:02 -0800

    [PBA-6085] Update to SDK Version v4.30.0_RC3

--- vendor/Ooyala/OoyalaSDK-tvOS/OoyalaSDK.framework/Versions/A/Headers/OOControls.h
+++ vendor/Ooyala/OoyalaSDK-tvOS/OoyalaSDK.framework/Versions/A/Headers/OOControls.h
@@ -14,6 +14,5 @@
 - (void)setVideoGravityButtonShowing:(BOOL)showing;
 - (void)setGravityState:(BOOL)isZoomed;
 - (void)setHiddenControls:(BOOL)hidden;
-- (void)updateTopBarSpacing:(CGFloat)height;
 
 @end
