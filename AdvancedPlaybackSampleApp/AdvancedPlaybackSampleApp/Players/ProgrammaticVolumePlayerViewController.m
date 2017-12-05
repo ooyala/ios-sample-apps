238920abdff21b7cc0dc4cc17eaca8be73694c63
MarkEPAM <mark_mizinov@epam.com>
2017-12-05 19:01:57 +0300

    [PLAYER-2394] Added iPhone X support and changed code style for AdvancedPlaybackSampleApp

--- AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/ProgrammaticVolumePlayerViewController.m
+++ AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/ProgrammaticVolumePlayerViewController.m
@@ -18,94 +18,99 @@
  *
  */
 @interface ProgrammaticVolumePlayerViewController ()
-@property (nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 
+#pragma mark - Private properties
+
+@property (nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 @property (nonatomic) NSString *embedCode;
 @property (nonatomic) NSString *nib;
 @property (nonatomic) NSString *pcode;
 @property (nonatomic) NSString *playerDomain;
+
 @end
+
 
 @implementation ProgrammaticVolumePlayerViewController
 AppDelegate *appDel;
 
+#pragma mark - Initialization
 
 - (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption qaModeEnabled:(BOOL)qaModeEnabled{
-    self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
-    self.nib = @"PlayerSimple";
-    if (self.playerSelectionOption) {
-        self.embedCode = self.playerSelectionOption.embedCode;
-        self.title = self.playerSelectionOption.title;
-        self.pcode = self.playerSelectionOption.pcode;
-        self.playerDomain = self.playerSelectionOption.domain;
-    } else {
-        NSLog(@"There was no PlayerSelectionOption!");
-        return nil;
-    }
-    return self;
+  self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
+  self.nib = @"PlayerSimple";
+  if (self.playerSelectionOption) {
+    self.embedCode = self.playerSelectionOption.embedCode;
+    self.title = self.playerSelectionOption.title;
+    self.pcode = self.playerSelectionOption.pcode;
+    self.playerDomain = self.playerSelectionOption.domain;
+  } else {
+    NSLog(@"There was no PlayerSelectionOption!");
+    return nil;
+  }
+  return self;
 }
 
+#pragma mark - Life cycle
+
 - (void)loadView {
-    [super loadView];
-    [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
+  [super loadView];
+  [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    appDel = [[UIApplication sharedApplication] delegate];
-    
-    // Create Ooyala ViewController
-    OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
-    self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
-    
-    
-    // Add self as an observer for the OoyalaPlayer
-    [[NSNotificationCenter defaultCenter] addObserver: self
-                                             selector:@selector(notificationHandler:)
-                                                 name:nil
-                                               object:self.ooyalaPlayerViewController.player];
-    // In QA Mode , making textView visible
-    if(self.qaModeEnabled==YES){
-        self.textView.hidden = NO;
-    }
-    
-    // Attach it to current view
-    [self addChildViewController:self.ooyalaPlayerViewController];
-    [self.playerView addSubview:self.ooyalaPlayerViewController.view];
-    [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
-    
-    // You can set the volume anytime after OOOoyalaPlayer is instantiated
-    [player setVolume:0.0f];
-    
-    // Load the video
-    [self.ooyalaPlayerViewController.player setEmbedCode:self.embedCode];
-    [self.ooyalaPlayerViewController.player play];
-    
+  [super viewDidLoad];
+  appDel = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+  
+  // Create Ooyala ViewController
+  OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode
+                                                          domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
+  self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
+  
+  // Add self as an observer for the OoyalaPlayer
+  [[NSNotificationCenter defaultCenter] addObserver: self
+                                           selector:@selector(notificationHandler:)
+                                               name:nil
+                                             object:self.ooyalaPlayerViewController.player];
+  // In QA Mode , making textView visible
+  self.textView.hidden = !self.qaModeEnabled;
+  
+  // Attach it to current view
+  [self addChildViewController:self.ooyalaPlayerViewController];
+  [self.playerView addSubview:self.ooyalaPlayerViewController.view];
+  [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
+  
+  // You can set the volume anytime after OOOoyalaPlayer is instantiated
+  [player setVolume:0.0f];
+  
+  // Load the video
+  [self.ooyalaPlayerViewController.player setEmbedCode:self.embedCode];
+  [self.ooyalaPlayerViewController.player play];
 }
 
 - (void) notificationHandler:(NSNotification*) notification {
-    
-    if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
-        
-        // Slowly increase the volume every tick of playback.
-        [self.ooyalaPlayerViewController.player setVolume: self.ooyalaPlayerViewController.player.volume + .025f];
-        return;
-    }
-    
-    NSString *message = [NSString stringWithFormat:@"Notification Received: %@. state: %@. playhead: %f count: %d",
-                         [notification name],
-                         [OOOoyalaPlayer playerStateToString:[self.ooyalaPlayerViewController.player state]],
-                         [self.ooyalaPlayerViewController.player playheadTime], appDel.count];
-    
-    NSLog(@"%@",message);
-    
-    //In QA Mode , adding notifications to the TextView
-    if(self.qaModeEnabled==YES) {
-        NSString *string = self.textView.text;
-        NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@" ,string, message];
-        [self.textView setText:appendString];
-    }
-    appDel.count++;
+  
+  if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
+  
+    // Slowly increase the volume every tick of playback.
+    [self.ooyalaPlayerViewController.player setVolume: self.ooyalaPlayerViewController.player.volume + .025f];
+    return;
+  }
+  
+  NSString *message = [NSString stringWithFormat:@"Notification Received: %@. state: %@. playhead: %f count: %d",
+                       [notification name],
+                       [OOOoyalaPlayer playerStateToString:[self.ooyalaPlayerViewController.player state]],
+                       [self.ooyalaPlayerViewController.player playheadTime], appDel.count];
+  
+  NSLog(@"%@",message);
+  
+  // In QA Mode , adding notifications to the TextView
+  if (self.qaModeEnabled == YES) {
+    NSString *string = self.textView.text;
+    NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@" ,string, message];
+    [self.textView setText:appendString];
+  }
+  appDel.count++;
 }
+
 
 @end
--- AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/UnbundledPlayerViewController.m
+++ AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/UnbundledPlayerViewController.m
@@ -6,96 +6,104 @@
  * @copyright  Copyright (c) 2015 Ooyala, Inc. All rights reserved.
  */
 
-
 #import "UnbundledPlayerViewController.h"
 #import <OoyalaSDK/OoyalaSDK.h>
 #import "AppDelegate.h"
 
-@interface UnbundledPlayerViewController ()
-@property (strong, nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 
+@interface UnbundledPlayerViewController ()
+
+#pragma mark - Private properties
+
+@property (strong, nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 @property NSString *assetUrl;
 @property NSString *nib;
 @property NSString *pcode;
 @property NSString *playerDomain;
+
 @end
+
 
 @implementation UnbundledPlayerViewController
 AppDelegate *appDel;
 
+#pragma mark - Initialization
+
 - (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption qaModeEnabled:(BOOL)qaModeEnabled {
-    self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
-    self.nib = @"PlayerSimple";
-    self.pcode =@"R2d3I6s06RyB712DN0_2GsQS-R-Y";
-    self.playerDomain = @"http://www.ooyala.com";
-    if (self.playerSelectionOption) {
-        self.assetUrl = self.playerSelectionOption.embedCode;
-        self.title = self.playerSelectionOption.title;
-        self.pcode = self.playerSelectionOption.pcode;
-        self.playerDomain = self.playerSelectionOption.domain;
-    }
-    return self;
+  self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
+  self.nib = @"PlayerSimple";
+  self.pcode =@"R2d3I6s06RyB712DN0_2GsQS-R-Y";
+  self.playerDomain = @"http://www.ooyala.com";
+  if (self.playerSelectionOption) {
+    self.assetUrl = self.playerSelectionOption.embedCode;
+    self.title = self.playerSelectionOption.title;
+    self.pcode = self.playerSelectionOption.pcode;
+    self.playerDomain = self.playerSelectionOption.domain;
+  }
+  return self;
 }
 
+#pragma mark - Life cycle
+
 - (void)loadView {
-    [super loadView];
-    [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
+  [super loadView];
+  [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
 }
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
-    
-    appDel = [[UIApplication sharedApplication] delegate];
-    
-    // Create Ooyala ViewController
-    OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
-    self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
-    
-    [[NSNotificationCenter defaultCenter] addObserver: self
-                                             selector:@selector(notificationHandler:)
-                                                 name:nil
-                                               object:self.ooyalaPlayerViewController.player];
-    // In QA Mode , making textView visible
-    if(self.qaModeEnabled==YES){
-        self.textView.hidden = NO;
-        
-    }
-    
-    // Attach it to current view
-    [self addChildViewController:self.ooyalaPlayerViewController];
-    [self.playerView addSubview:self.ooyalaPlayerViewController.view];
-    [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
-    
-    // Load the video
-    OOStream *s = [[OOStream alloc]
-                   initWithUrl:[NSURL URLWithString:self.assetUrl]
-                   deliveryType:OO_DELIVERY_TYPE_HLS];
-    OOUnbundledVideo *v = [[OOUnbundledVideo alloc] initWithUnbundledStreams:@[s]];
-    [self.ooyalaPlayerViewController.player setUnbundledVideo:v];
-    [self.ooyalaPlayerViewController.player play];
+  [super viewDidLoad];
+  
+  appDel = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+  
+  // Create Ooyala ViewController
+  OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode
+                                                          domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
+  self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
+  
+  [[NSNotificationCenter defaultCenter] addObserver:self
+                                           selector:@selector(notificationHandler:)
+                                               name:nil
+                                             object:self.ooyalaPlayerViewController.player];
+  // In QA Mode , making textView visible
+  self.textView.hidden = !self.qaModeEnabled;
+  
+  // Attach it to current view
+  [self addChildViewController:self.ooyalaPlayerViewController];
+  [self.playerView addSubview:self.ooyalaPlayerViewController.view];
+  [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
+  
+  // Load the video
+  OOStream *stream = [[OOStream alloc] initWithUrl:[NSURL URLWithString:self.assetUrl]
+                                      deliveryType:OO_DELIVERY_TYPE_HLS];
+  
+  OOUnbundledVideo *unbundledVideo = [[OOUnbundledVideo alloc] initWithUnbundledStreams:@[stream]];
+  
+  [self.ooyalaPlayerViewController.player setUnbundledVideo:unbundledVideo];
+  [self.ooyalaPlayerViewController.player play];
 }
 
 - (void) notificationHandler:(NSNotification*) notification {
-    
-    // Ignore TimeChangedNotificiations for shorter logs
-    if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
-        return;
-    }
-    
-    NSString *message = [NSString stringWithFormat:@"Notification Received: %@. state: %@. playhead: %f count: %d",
-                         [notification name],
-                         [OOOoyalaPlayer playerStateToString:[self.ooyalaPlayerViewController.player state]],
-                         [self.ooyalaPlayerViewController.player playheadTime], appDel.count];
-    
-    NSLog(@"%@",message);
-    
-    //In QA Mode , adding notifications to the TextView
-    if(self.qaModeEnabled==YES) {
-        NSString *string = self.textView.text;
-        NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@" ,string, message];
-        [self.textView setText:appendString];
-    }
-    appDel.count++;
+  
+  // Ignore TimeChangedNotificiations for shorter logs
+  if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
+      return;
+  }
+  
+  NSString *message = [NSString stringWithFormat:@"Notification Received: %@. state: %@. playhead: %f count: %d",
+                       [notification name],
+                       [OOOoyalaPlayer playerStateToString:[self.ooyalaPlayerViewController.player state]],
+                       [self.ooyalaPlayerViewController.player playheadTime], appDel.count];
+  
+  NSLog(@"%@",message);
+  
+  //In QA Mode , adding notifications to the TextView
+  if (self.qaModeEnabled == YES) {
+    NSString *string = self.textView.text;
+    NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@" ,string, message];
+    [self.textView setText:appendString];
+  }
+  appDel.count++;
 }
+
 
 @end
