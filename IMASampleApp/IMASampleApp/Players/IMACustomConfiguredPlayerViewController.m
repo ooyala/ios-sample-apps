2f71f895218ccc2de40e8c37146d4b51b68dd6a5
Mark Mizinov <Mark_Mizinov@epam.com>
2017-12-06 14:34:08 +0300

    [PLAYER-2394] Added iPhone X support and fix code style for IMASampleApp

--- IMASampleApp/IMASampleApp/Players/IMACustomConfiguredPlayerViewController.m
+++ IMASampleApp/IMASampleApp/Players/IMACustomConfiguredPlayerViewController.m
@@ -6,25 +6,32 @@
  * @copyright  Copyright (c) 2014 Ooyala, Inc. All rights reserved.
  */
 
-
 #import "IMACustomConfiguredPlayerViewController.h"
 #import <OoyalaSDK/OoyalaSDK.h>
 #import <OoyalaIMASDK/OOIMAManager.h>
 #import <OoyalaIMASDK/OOIMAConfiguration.h>
 #import "AppDelegate.h"
 
+
 @interface IMACustomConfiguredPlayerViewController ()
+
+#pragma mark - Private functions
+
 @property (nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 @property (nonatomic) OOIMAManager *adsManager;
 @property (nonatomic) NSString *embedCode;
 @property (nonatomic) NSString *nib;
 @property (nonatomic) NSString *pcode;
 @property (nonatomic) NSString *playerDomain;
+
 @end
+
 
 @implementation IMACustomConfiguredPlayerViewController{
   AppDelegate *appDel;
 }
+
+#pragma mark - Initialization
 
 - (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption qaModeEnabled:(BOOL)qaModeEnabled {
   self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
@@ -43,6 +50,8 @@
   return self;
 }
 
+#pragma mark - Life cycle
+
 - (void)loadView {
   [super loadView];
   [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
@@ -50,24 +59,21 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
-  appDel = [[UIApplication sharedApplication] delegate];
+  appDel = (AppDelegate *)[[UIApplication sharedApplication] delegate];
 
   // Create Ooyala ViewController
-  OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
+  OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode
+                                                          domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
   self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
 
-  [[NSNotificationCenter defaultCenter] addObserver: self
+  [[NSNotificationCenter defaultCenter] addObserver:self
                                            selector:@selector(notificationHandler:)
                                                name:nil
                                              object:_ooyalaPlayerViewController.player];
   
   // In QA Mode , making textView visible
-  if(self.qaModeEnabled==YES){
-    self.textView.hidden = NO;
-    
-  }
+  self.textView.hidden = !self.qaModeEnabled;
   
-
   // Attach it to current view
   [self addChildViewController:_ooyalaPlayerViewController];
   [self.playerView addSubview:_ooyalaPlayerViewController.view];
@@ -82,12 +88,13 @@
   // Override the ad url to load when a video is loaded
   [self.adsManager setAdUrlOverride:@"http://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/7521029/pb_test_mid&ciu_szs=640x480&impl=s&cmsid=949&vid=FjbGRjbzp0DV_5-NtXBVo5Rgp3Sj0R5C&gdfp_req=1&env=vp&output=xml_vast2&unviewed_position_start=1&url=[referrer_url]&description_url=[description_url]&correlator=[timestamp]"];
 
-
   // Load the video
   [_ooyalaPlayerViewController.player setEmbedCode:self.embedCode];
 }
 
-- (void) notificationHandler:(NSNotification*) notification {
+#pragma mark - Private functions
+
+- (void)notificationHandler:(NSNotification*) notification {
 
   // Ignore TimeChangedNotificiations for shorter logs
   if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
@@ -101,14 +108,15 @@
   
   NSLog(@"%@",message);
 
-  //In QA Mode , adding notifications to the TextView
-  if(self.qaModeEnabled==YES) {
+  // In QA Mode , adding notifications to the TextView
+  if (self.qaModeEnabled == YES) {
     NSString *string = self.textView.text;
     NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@",string,message];
     [self.textView setText:appendString];
-    
   }
+  
   appDel.count++;
 }
+
 
 @end
