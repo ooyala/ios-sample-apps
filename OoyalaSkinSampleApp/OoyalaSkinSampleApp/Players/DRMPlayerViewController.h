d70ca4667fd146638b0ae3be86c4c3cb44eafb8f
Ivan Sakharovskii <30658883+IvanSakharovskii@users.noreply.github.com>
2018-07-13 11:10:12 +0300

    [PLAYER-3845] Added DRM support for OoyalaSkinSampleApp (multi audio section) (#288)

---
+++ OoyalaSkinSampleApp/OoyalaSkinSampleApp/Players/DRMPlayerViewController.h
@@ -0,0 +1,16 @@
+//
+//  DRMPlayerViewController.h
+//  OoyalaSkinSampleApp
+//
+//  Copyright © 2018 Ooyala, Inc. All rights reserved.
+//
+
+#import "SampleAppPlayerViewController.h"
+
+
+@interface DRMPlayerViewController : SampleAppPlayerViewController
+
+@property (nonatomic, retain) IBOutlet UIView *videoView;
+@property (nonatomic, retain) IBOutlet UILabel *textLabel;
+
+@end
