f851aa3b18c44f48aab0340f39804f80070ede0e
Carlos Ceja Limón <cceja@ooyala.com>
2018-07-27 14:16:58 -0500

    [PLAYER-3932] Remove Typo

--- OoyalaSSAISampleApp/OoyalaSSAISampleApp/SupportFiles/OoyalaSSAISampleApp-Bridging-Header.h
+++ OoyalaSSAISampleApp/OoyalaSSAISampleApp/SupportFiles/OoyalaSSAISampleApp-Bridging-Header.h
@@ -1,15 +1,15 @@
 //
-//  OoyalaSSAISampleApp-Bridging-Header..h
+//  OoyalaSSAISampleApp-Bridging-Header.h
 //  OoyalaSSAISampleApp
 //
 //  Copyright © 2018 Ooyala. All rights reserved.
 //
 
-#ifndef OoyalaSSAISampleApp_Bridging_Header__h
-#define OoyalaSSAISampleApp_Bridging_Header__h
+#ifndef OoyalaSSAISampleApp_Bridging_Header_h
+#define OoyalaSSAISampleApp_Bridging_Header_h
 
 #import <OoyalaSDK/OoyalaSDK.h>
 #import <OoyalaSSAISDK/OoyalaSSAISDK.h>
 #import <OoyalaSkinSDK/OoyalaSkinSDK.h>
 
-#endif /* OoyalaSSAISampleApp_Bridging_Header__h */
+#endif /* OoyalaSSAISampleApp_Bridging_Header_h */
