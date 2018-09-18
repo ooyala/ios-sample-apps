e1d7a788d866a17fa0518faabd5fc2ad9a015cbb
Eric Vargas <evargas@ooyala.com>
2018-09-18 13:17:56 -0700

    [DDMM] Preparing the player code

---
+++ PlaybackLab/DDMMSampleApp/DDMMSampleApp/DeleteViewController.h
@@ -0,0 +1,17 @@
+//
+//  DeleteViewController.h
+//  DDMMSampleApp
+//
+//  Created by Eric Vargas on 9/18/18.
+//  Copyright © 2018 Ooyala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface DeleteViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
--- PlaybackLab/DDMMSampleApp/DDMMSampleApp/ViewController.swift
+++ PlaybackLab/DDMMSampleApp/DDMMSampleApp/ViewController.swift
@@ -7,14 +7,40 @@
 //
 
 import UIKit
+import CapsGeneratorFramework
 
 class ViewController: UIViewController {
 
   @IBOutlet weak var playerView: UIView!
+  let pcode = "c0cTkxOqALQviQIGAHWY5hP0q9gU"
+  let embedCode = "Y1ZHB1ZDqfhCPjYYRbCEOz0GR8IsVRm1"
+  let domain = "http://www.ooyala.com"
   
   override func viewDidLoad() {
     super.viewDidLoad()
   }
   
+  func setupOoyalaPlayer() {
+    
+    let playerInfo = MyPlayerInfo()
+    let myParams = ["ddmm_device_param": "myVal"]
+    playerInfo.additionalParams = myParams
+    let options = OOOptions()
+    options?.playerInfo = playerInfo
+    let player = OOOoyalaPlayer(pcode: self.pcode, domain: OOPlayerDomain(string: self.domain), options: options)
+  }
 }
 
+class MyPlayerInfo: OODefaultPlayerInfo {
+  var myOverridenParams: [AnyHashable: Any]?
+  
+  override var additionalParams: [AnyHashable : Any]? {
+    get {
+      return myOverridenParams
+    }
+    
+    set {
+      myOverridenParams = newValue
+    }
+  }
+}
