6e69993f8d8844b124639d6f43bf92136a12f662
Eric Vargas <evargas@ooyala.com>
2018-09-18 12:32:56 -0700

    [DDMM] Build DDMMSampleApp

---
+++ PlaybackLab/DDMMSampleApp/DDMMSampleApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DDMMSampleApp
+//
+//  Created by Eric Vargas on 9/18/18.
+//  Copyright © 2018 Ooyala. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+
+}
+
---
+++ PlaybackLab/DDMMSampleApp/VendorLibraries/CapsGeneratorFramework.framework/Headers/CapsGeneratorFramework.h
@@ -0,0 +1,19 @@
+//
+//  CapsGeneratorFramework.h
+//  CapsGeneratorFramework
+//
+//  Created by Isha Pandya on 10/9/18.
+//  Copyright © 2018 Isha Pandya. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CapsGeneratorFramework.
+FOUNDATION_EXPORT double CapsGeneratorFrameworkVersionNumber;
+
+//! Project version string for CapsGeneratorFramework.
+FOUNDATION_EXPORT const unsigned char CapsGeneratorFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CapsGeneratorFramework/PublicHeader.h>
+
+
