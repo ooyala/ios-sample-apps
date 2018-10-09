444ac9510c8143eee326d3c6d0c5681999047aec
Eric Vargas <evargas@ooyala.com>
2018-10-09 15:02:38 -0700

    Use OoyalaSDK from root vendor folder

--- PlaybackLab/DDMMOoyalaSampleApp/Vendor/OoyalaSDK.framework/Versions/A/Headers/OOStreamPlayerMappingPredicate.h
+++
@@ -1,9 +0,0 @@
-//
-//Copyright (c) 2015 Ooyala, Inc. All rights reserved.
-//
-
-#import <Foundation/Foundation.h>
-
-@protocol OOStreamPlayerMappingPredicate <NSCopying>
--(BOOL) matchesStreams:(NSArray*)streams;
-@end
