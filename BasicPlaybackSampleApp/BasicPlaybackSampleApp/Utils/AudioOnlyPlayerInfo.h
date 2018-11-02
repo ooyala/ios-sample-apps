abd5d22a7bdce57012c53473e0d9608fbcc43466
Max Kupetskii <maksim_kupetskii@epam.com>
2018-11-02 17:17:34 +0300

    [PLAYER-4647] AudioOnlyPlayerInfo added
    -project updated

---
+++ BasicPlaybackSampleApp/BasicPlaybackSampleApp/Utils/AudioOnlyPlayerInfo.h
@@ -0,0 +1,13 @@
+//
+//  AudioOnlyPlayerInfo.h
+//  BasicPlaybackSampleApp
+//
+//  Created by Max Kupetskii on 11/2/18.
+//  Copyright © 2018 Ooyala, Inc. All rights reserved.
+//
+
+#import <OoyalaSDK/OODefaultPlayerInfo.h>
+
+@interface AudioOnlyPlayerInfo : OODefaultPlayerInfo
+
+@end
---
+++ BasicPlaybackSampleApp/BasicPlaybackSampleApp/Utils/AudioOnlyPlayerInfo.m
@@ -0,0 +1,17 @@
+//
+//  AudioOnlyPlayerInfo.m
+//  BasicPlaybackSampleApp
+//
+//  Created by Max Kupetskii on 11/2/18.
+//  Copyright © 2018 Ooyala, Inc. All rights reserved.
+//
+
+#import "AudioOnlyPlayerInfo.h"
+
+@implementation AudioOnlyPlayerInfo
+
+- (BOOL)isAudioOnly {
+  return YES;
+}
+
+@end
