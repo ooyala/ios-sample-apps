ed468bb211a376da45bf550041de2324034244f7
QA Tools - Alfred Jenkins <qa-tools-oncall@ooyala.com>
2018-12-26 15:29:41 +0300

    [PBA-6602] Update to SDK Version v4.42.1_RC12

--- vendor/Ooyala/OoyalaCastSDK-iOS/OoyalaCastSDK.framework/Versions/A/Headers/OOCastMiniControllerProtocol.h
+++ vendor/Ooyala/OoyalaCastSDK-iOS/OoyalaCastSDK.framework/Versions/A/Headers/OOCastMiniControllerProtocol.h
@@ -9,20 +9,21 @@
 #import <Foundation/Foundation.h>
 
 /**
- * Should maintain a OOCastMiniControllerDelegate.
+ Should maintain a OOCastMiniControllerDelegate.
  */
 @protocol OOCastMiniControllerProtocol <NSObject>
 
 /**
- * This is called when the play/pause button need to update by OOCastManager
- * Show pause button if the given state is OOOoyalaPlayerStatePlaying
+ This is called when the play/pause button need to update by OOCastManager
+
+ @param isPlaying YES if state is playing
  */
 - (void)updatePlayState:(BOOL)isPlaying;
 
 /**
- * This is called when we disconnect from the chromecast device.
- * Can also be used e.g. by app code to explicitly remove the MiniController.
- * This should call [delegate onDismissMiniController:] as well.
+ This is called when we disconnect from the chromecast device.
+ Can also be used e.g. by app code to explicitly remove the MiniController.
+ This should call [delegate onDismissMiniController:] as well.
  */
 - (void)dismiss;
 
