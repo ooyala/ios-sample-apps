098164ecc4a9877edc8c535f2a8cf12c2bcd74bb
Max Kupetskii <maksim_kupetskii@epam.com>
2019-01-21 10:20:29 +0300

    [PLAYER-5039] OoyalaSkinListViewController updated

--- OoyalaSkinSampleApp/OoyalaSkinSampleApp/Utils/OoyalaSkinListViewController.m
+++ OoyalaSkinSampleApp/OoyalaSkinSampleApp/Utils/OoyalaSkinListViewController.m
@@ -12,28 +12,12 @@
 #import "PlayerSelectionOption.h"
 
 @interface OoyalaSkinListViewController ()
-@property (nonatomic) NSMutableArray *options;
+
 @property (nonatomic) BOOL qaLogEnabled;
-@property (nonatomic) NSMutableArray *optionList;
-@property (nonatomic) NSMutableArray *optionEmbedCodes;
+
 @end
 
 @implementation OoyalaSkinListViewController
-
-- (id)init {
-  self = [super init];
-  self.title = @"Ooyala Skin Sample App";
-  return self;
-}
-
--(void)addCommonWithTitle:(NSString*)title embedCode:(NSString*)embedCode pcode:(NSString *)pcode {
-  [self insertNewObject: [[PlayerSelectionOption alloc] initWithTitle:title
-                                                            embedCode:embedCode
-                                                                pcode:pcode
-                                                         playerDomain:@"http://www.ooyala.com"
-                                                       viewController: [DefaultSkinPlayerViewController class]
-                                                                  nib:@"DefaultSkinPlayerView"]];
-}
 
 - (void)addTestCases {
   // subclasses need to override this to add test cases.
@@ -41,38 +25,32 @@
 
 - (void)viewDidLoad {
   [super viewDidLoad];
+
   self.navigationController.navigationBar.translucent = NO;
-  [self.tableView registerNib:[UINib nibWithNibName:@"TableCell" bundle:nil]forCellReuseIdentifier:@"TableCell"];
+  self.title = @"Ooyala Skin Sample App";
+  [self.tableView registerNib:[UINib nibWithNibName:@"TableCell" bundle:nil]
+       forCellReuseIdentifier:@"TableCell"];
   
-  UISwitch *swtLog = [[UISwitch alloc] init];
-  [swtLog addTarget:self action:@selector(changeSwitch:) forControlEvents:UIControlEventValueChanged];
-  UILabel *lblLog = [[UILabel alloc]  initWithFrame:CGRectMake(0,0,44,44)];
-  [lblLog setText:@"QA"];
+  UISwitch *swtLog = [UISwitch new];
+  [swtLog addTarget:self
+             action:@selector(changeSwitch:)
+   forControlEvents:UIControlEventValueChanged];
+  UILabel *lblLog = [[UILabel alloc] initWithFrame:CGRectMake(0,0,44,44)];
+  lblLog.text = @"QA";
   
   UIBarButtonItem * btn = [[UIBarButtonItem alloc] initWithCustomView:swtLog];
   UIBarButtonItem * lbl = [[UIBarButtonItem alloc] initWithCustomView:lblLog];
-  self.navigationItem.rightBarButtonItems = [NSArray arrayWithObjects:btn,lbl, nil] ;
-  [self.tableView registerNib:[UINib nibWithNibName:@"TableCell" bundle:nil]forCellReuseIdentifier:@"TableCell"];
-  
+  self.navigationItem.rightBarButtonItems = @[btn, lbl];
+
   [self addTestCases];
 }
+
 - (void)changeSwitch:(id)sender{
-  if([sender isOn]){
+  if ([sender isOn]) {
     self.qaLogEnabled = YES;
   } else{
     self.qaLogEnabled = NO;
   }
-}
-
-- (void)insertNewObject:(PlayerSelectionOption *)selectionObject {
-  if (!self.options) {
-    self.options = [[NSMutableArray alloc] init];
-  }
-  [self.options addObject:selectionObject];
-}
-
-- (void)didReceiveMemoryWarning {
-  [super didReceiveMemoryWarning];
 }
 
 #pragma mark - Table View
@@ -86,10 +64,10 @@
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableCell" forIndexPath:indexPath];
-  
+  UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"TableCell"
+                                                          forIndexPath:indexPath];
   PlayerSelectionOption *selection = self.options[indexPath.row];
-  cell.textLabel.text = [selection title];
+  cell.textLabel.text = selection.title;
   return cell;
 }
 
@@ -97,11 +75,11 @@
   return NO;
 }
 
-- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
-{
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
   // When a row is selected, load its desired PlayerViewController
   PlayerSelectionOption *selection = self.options[indexPath.row];
-  SampleAppPlayerViewController *controller = [(SampleAppPlayerViewController *)[[selection viewController] alloc] initWithPlayerSelectionOption:selection qaModeEnabled:self.qaLogEnabled];
+  SampleAppPlayerViewController *controller = [(SampleAppPlayerViewController *)[selection.viewController alloc] initWithPlayerSelectionOption:selection qaModeEnabled:self.qaLogEnabled];
   [self.navigationController pushViewController:controller animated:YES];
 }
+
 @end
