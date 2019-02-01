54af1df914197bf7d1812c0e35a6c576983aa650
Max Kupetskii <maksim_kupetskii@epam.com>
2019-02-01 11:15:07 +0300

    [PLAYER-5090] Players code style applied

--- AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/LocalizationLanguagesViewController.m
+++ AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/LocalizationLanguagesViewController.m
@@ -9,7 +9,7 @@
 #import "LocalizationLanguagesViewController.h"
 #import <OoyalaSDK/OoyalaSDK.h>
 #import "AppDelegate.h"
-
+#import "PlayerSelectionOption.h"
 
 @interface LocalizationLanguagesViewController ()
 
@@ -23,21 +23,22 @@
 
 @end
 
-
 @implementation LocalizationLanguagesViewController{
-    AppDelegate *appDel;
+  AppDelegate *appDel;
 }
 
 #pragma mark - Initializaiton
 
-- (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption qaModeEnabled:(BOOL)qaModeEnabled{
-  self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
-  self.nib = @"PlayerSimple";
+- (instancetype)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption
+                                qaModeEnabled:(BOOL)qaModeEnabled{
+  self = [super initWithPlayerSelectionOption:playerSelectionOption
+                                qaModeEnabled:qaModeEnabled];
+  _nib = @"PlayerSimple";
   if (self && self.playerSelectionOption) {
-    _embedCode = self.playerSelectionOption.embedCode;
-    self.title = self.playerSelectionOption.title;
-    _pcode = self.playerSelectionOption.pcode;
+    _embedCode    = self.playerSelectionOption.embedCode;
+    _pcode        = self.playerSelectionOption.pcode;
     _playerDomain = self.playerSelectionOption.domain;
+    self.title    = self.playerSelectionOption.title;
   } else {
     NSLog(@"There was no PlayerSelectionOption!");
     return nil;
@@ -49,35 +50,35 @@
 
 - (void)loadView {
   [super loadView];
-  [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
+  [NSBundle.mainBundle loadNibNamed:self.nib owner:self options:nil];
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  appDel = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+  appDel = (AppDelegate *)UIApplication.sharedApplication.delegate;
   
   // Create Ooyala ViewController
   OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode
                                                           domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
   self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
   
-  [[NSNotificationCenter defaultCenter] addObserver: self
-                                           selector:@selector(notificationHandler:)
-                                               name:nil
-                                             object:self.ooyalaPlayerViewController.player];
+  [NSNotificationCenter.defaultCenter addObserver:self
+                                         selector:@selector(notificationHandler:)
+                                             name:nil
+                                           object:self.ooyalaPlayerViewController.player];
   
   // In QA Mode , making textView visible
   self.textView.hidden = !self.qaModeEnabled;
   
   // Add french localization strings and modify some english
-  NSDictionary *updatedLocalization = [self updateLocalizedLanguages:[OOOoyalaPlayerViewController availableLocalizations]];
+  NSDictionary *updatedLocalization = [self updateLocalizedLanguages:OOOoyalaPlayerViewController.availableLocalizations];
   [OOOoyalaPlayerViewController setAvailableLocalizations:updatedLocalization];
   
   // Attach it to current view
   [self addChildViewController:self.ooyalaPlayerViewController];
   [self.playerView addSubview:self.ooyalaPlayerViewController.view];
-  [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
+  self.ooyalaPlayerViewController.view.frame = self.playerView.bounds;
   
   if ([self.ooyalaPlayerViewController.player setEmbedCode:self.embedCode]) {
     [self.ooyalaPlayerViewController.player play];
@@ -103,28 +104,27 @@
   return mutableLanguages;
 }
 
-- (void)notificationHandler:(NSNotification*) notification {
+- (void)notificationHandler:(NSNotification *)notification {
   if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
     return;
   }
   
   NSString *message = [NSString stringWithFormat:@"Notification Received: %@. state: %@. playhead: %f count: %d",
-                       [notification name],
-                       [OOOoyalaPlayerStateConverter playerStateToString:[self.ooyalaPlayerViewController.player state]],
-                       [self.ooyalaPlayerViewController.player playheadTime], appDel.count];
-  
+                       notification.name,
+                       [OOOoyalaPlayerStateConverter playerStateToString:self.ooyalaPlayerViewController.player.state],
+                       self.ooyalaPlayerViewController.player.playheadTime,
+                       appDel.count];
   NSLog(@"%@",message);
   
   // In QA Mode , adding notifications to the TextView
   if (self.qaModeEnabled) {
     NSString *string = self.textView.text;
-    NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@" ,string, message];
+    NSString *appendString = [NSString stringWithFormat:@"%@ :::::::::: %@", string, message];
     dispatch_async(dispatch_get_main_queue(), ^{
       self.textView.text = appendString;
     });
   }
   appDel.count++;
 }
-
 
 @end
--- AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/UnbundledPlayerViewController.m
+++ AdvancedPlaybackSampleApp/AdvancedPlaybackSampleApp/Players/UnbundledPlayerViewController.m
@@ -9,13 +9,13 @@
 #import "UnbundledPlayerViewController.h"
 #import <OoyalaSDK/OoyalaSDK.h>
 #import "AppDelegate.h"
-
+#import "PlayerSelectionOption.h"
 
 @interface UnbundledPlayerViewController ()
 
 #pragma mark - Private properties
 
-@property (strong, nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
+@property (nonatomic) OOOoyalaPlayerViewController *ooyalaPlayerViewController;
 @property NSString *assetUrl;
 @property NSString *nib;
 @property NSString *pcode;
@@ -30,16 +30,17 @@
 
 #pragma mark - Initialization
 
-- (id)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption qaModeEnabled:(BOOL)qaModeEnabled {
+- (instancetype)initWithPlayerSelectionOption:(PlayerSelectionOption *)playerSelectionOption
+                                qaModeEnabled:(BOOL)qaModeEnabled {
   self = [super initWithPlayerSelectionOption: playerSelectionOption qaModeEnabled:qaModeEnabled];
-  self.nib = @"PlayerSimple";
-  self.pcode =@"R2d3I6s06RyB712DN0_2GsQS-R-Y";
-  self.playerDomain = @"http://www.ooyala.com";
+  _nib          = @"PlayerSimple";
+  _pcode        = @"R2d3I6s06RyB712DN0_2GsQS-R-Y";
+  _playerDomain = @"http://www.ooyala.com";
   if (self.playerSelectionOption) {
-    self.assetUrl = self.playerSelectionOption.embedCode;
-    self.title = self.playerSelectionOption.title;
-    self.pcode = self.playerSelectionOption.pcode;
-    self.playerDomain = self.playerSelectionOption.domain;
+    assetUrl     = self.playerSelectionOption.embedCode;
+    pcode        = self.playerSelectionOption.pcode;
+    playerDomain = self.playerSelectionOption.domain;
+    self.title   = self.playerSelectionOption.title;
   }
   return self;
 }
@@ -48,30 +49,30 @@
 
 - (void)loadView {
   [super loadView];
-  [[NSBundle mainBundle] loadNibNamed:self.nib owner:self options:nil];
+  [NSBundle.mainBundle loadNibNamed:self.nib owner:self options:nil];
 }
 
 - (void)viewDidLoad {
   [super viewDidLoad];
   
-  appDel = (AppDelegate *)[[UIApplication sharedApplication] delegate];
+  appDel = (AppDelegate *)UIApplication.sharedApplication.delegate;
   
   // Create Ooyala ViewController
   OOOoyalaPlayer *player = [[OOOoyalaPlayer alloc] initWithPcode:self.pcode
                                                           domain:[[OOPlayerDomain alloc] initWithString:self.playerDomain]];
-  self.ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
+  _ooyalaPlayerViewController = [[OOOoyalaPlayerViewController alloc] initWithPlayer:player];
   
-  [[NSNotificationCenter defaultCenter] addObserver:self
-                                           selector:@selector(notificationHandler:)
-                                               name:nil
-                                             object:self.ooyalaPlayerViewController.player];
+  [NSNotificationCenter.defaultCenter addObserver:self
+                                         selector:@selector(notificationHandler:)
+                                             name:nil
+                                           object:self.ooyalaPlayerViewController.player];
   // In QA Mode , making textView visible
   self.textView.hidden = !self.qaModeEnabled;
   
   // Attach it to current view
   [self addChildViewController:self.ooyalaPlayerViewController];
   [self.playerView addSubview:self.ooyalaPlayerViewController.view];
-  [self.ooyalaPlayerViewController.view setFrame:self.playerView.bounds];
+  self.ooyalaPlayerViewController.view.frame = self.playerView.bounds;
   
   // Load the video
   OOStream *stream = [[OOStream alloc] initWithUrl:[NSURL URLWithString:self.assetUrl]
@@ -83,17 +84,17 @@
   [self.ooyalaPlayerViewController.player play];
 }
 
-- (void) notificationHandler:(NSNotification*) notification {
-  
+- (void)notificationHandler:(NSNotification *)notification {
   // Ignore TimeChangedNotificiations for shorter logs
   if ([notification.name isEqualToString:OOOoyalaPlayerTimeChangedNotification]) {
       return;
   }
   
   NSString *message = [NSString stringWithFormat:@"Notification Received: %@. state: %@. playhead: %f count: %d",
-                       [notification name],
-                       [OOOoyalaPlayerStateConverter playerStateToString:[self.ooyalaPlayerViewController.player state]],
-                       [self.ooyalaPlayerViewController.player playheadTime], appDel.count];
+                       notification.name,
+                       [OOOoyalaPlayerStateConverter playerStateToString:self.ooyalaPlayerViewController.player.state],
+                       self.ooyalaPlayerViewController.player.playheadTime,
+                       appDel.count];
   
   NSLog(@"%@",message);
   
@@ -107,6 +108,5 @@
   }
   appDel.count++;
 }
-
 
 @end
