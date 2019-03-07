cfea6aa88280ee4e4a5bc4abbdcb1357cccbad73
Oleksandr Sulyma <Oleksandr_Sulyma@epam.com>
2019-03-07 18:28:54 +0200

    [PLAYER-5077] Added correct name for JS-button into json. Added helper URL for localhost

--- PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleApp/Players/DefaultSkinPlayerViewController.m
+++ PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleApp/Players/DefaultSkinPlayerViewController.m
@@ -71,6 +71,7 @@
   
   OODiscoveryOptions *discoveryOptions = [[OODiscoveryOptions alloc] initWithType:OODiscoveryTypePopular limit:10 timeout:60];
   NSURL *jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  //NSURL *jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios"];
   NSDictionary *overrideConfigs = @{@"upNextScreen": @{@"timeToShow": @"8"}};
 
   //Configure the button
