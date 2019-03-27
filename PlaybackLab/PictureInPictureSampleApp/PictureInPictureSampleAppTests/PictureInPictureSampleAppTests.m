4669cd31e11e30648d7c1b114368b59ac1a4dab5
Oleksandr Sulyma <Oleksandr_Sulyma@epam.com>
2019-03-27 18:21:52 +0200

    [PLAYER-5411] Added Unit test target for Picture-in-picture sample app

---
+++ PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/PictureInPictureSampleAppTests.m
@@ -0,0 +1,37 @@
+//
+//  PictureInPictureSampleAppTests.m
+//  PictureInPictureSampleAppTests
+//
+//  Created by Oleksandr Sulyma on 3/27/19.
+//  Copyright © 2019 Ooyala, Inc. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PictureInPictureSampleAppTests : XCTestCase
+
+@end
+
+@implementation PictureInPictureSampleAppTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
