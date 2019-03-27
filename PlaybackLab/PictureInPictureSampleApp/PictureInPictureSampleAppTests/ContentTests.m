3943d463eab0503ce29e8b06178233b780baa4b9
Oleksandr Sulyma <Oleksandr_Sulyma@epam.com>
2019-03-27 18:58:11 +0200

    [PLAYER-5411] Added test for checking if icons for pip-buttons exist in config

--- PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/ContentTests.m
+++ PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/ContentTests.m
@@ -26,10 +26,25 @@
 - (void)testSkinConfigJson {
   
   // 1 - given
+  
   // 2 - when
   
   // 3 - then
   XCTAssertGreaterThan(self.skinConfig.count, 0, @"❌ skin.json can't be empty");
+}
+
+- (void)testSkinPiPButtons {
+  
+  // 1 - given
+  
+  // 2 - when
+  NSDictionary *iconsDict = self.skinConfig[@"icons"];
+  NSDictionary *pipInButton = iconsDict[@"pipInButton"];
+  NSDictionary *pipOutButton = iconsDict[@"pipOutButton"];
+  
+  // 3 - then
+  XCTAssertGreaterThan(pipInButton.count, 0, @"❌ pipInButton can't be empty");
+  XCTAssertGreaterThan(pipOutButton.count, 0, @"❌ pipOutButton can't be empty");
 }
 
 + (NSDictionary *)getTestDictionary:(NSString *)file {
