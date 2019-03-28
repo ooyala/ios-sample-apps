a7f6d59104982ae1f048c821f49c81a377612cd9
Oleksandr Sulyma <Oleksandr_Sulyma@epam.com>
2019-03-28 11:04:48 +0200

    [PLAYER-5411] Added test for checking if button for pip defined and configured

--- PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/ContentTests.m
+++ PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/ContentTests.m
@@ -33,18 +33,34 @@
   XCTAssertGreaterThan(self.skinConfig.count, 0, @"❌ skin.json can't be empty");
 }
 
+
 - (void)testSkinPiPButtons {
   
   // 1 - given
   
   // 2 - when
+  // 2-1 Check if button for pip defined and configured
+  NSDictionary *buttonsDict = self.skinConfig[@"buttons"];
+  NSArray<NSDictionary *> *buttonsMobile = buttonsDict[@"mobileContent"];
+  BOOL foundFlag = false;
+  for (NSDictionary *nextDict in buttonsMobile) {
+    NSString *buttonName = nextDict[@"name"];
+
+    if ([buttonName isEqualToString:@"pipButton"]) {
+      foundFlag = true;
+      break;
+    }
+  }
+  
+  // 2-2 Check if icon for button defined
   NSDictionary *iconsDict = self.skinConfig[@"icons"];
-  NSDictionary *pipInButton = iconsDict[@"pipInButton"];
-  NSDictionary *pipOutButton = iconsDict[@"pipOutButton"];
+  NSDictionary *pipInButtonIcon = iconsDict[@"pipInButton"];
+  NSDictionary *pipOutButtonIcon = iconsDict[@"pipOutButton"];
   
   // 3 - then
-  XCTAssertGreaterThan(pipInButton.count, 0, @"❌ pipInButton can't be empty");
-  XCTAssertGreaterThan(pipOutButton.count, 0, @"❌ pipOutButton can't be empty");
+  XCTAssertTrue(foundFlag, @"❌ pipInButton dictionary is not found");
+  XCTAssertGreaterThan(pipInButtonIcon.count, 0, @"❌ pipInButtonIcon dictionary can't be empty");
+  XCTAssertGreaterThan(pipOutButtonIcon.count, 0, @"❌ pipOutButtonIcon dictionary can't be empty");
 }
 
 + (NSDictionary *)getTestDictionary:(NSString *)file {
--- PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/PictureInPictureSampleAppTests.m
+++ PlaybackLab/PictureInPictureSampleApp/PictureInPictureSampleAppTests/PictureInPictureSampleAppTests.m
@@ -21,9 +21,5 @@
     // Put teardown code here. This method is called after the invocation of each test method in the class.
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
-}
 
 @end
