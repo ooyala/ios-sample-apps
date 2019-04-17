61506b60f8960bc13b0e5480f5f078c4027ac98a
Max Kupetskii <maksim_kupetskii@epam.com>
2019-04-17 11:41:06 +0300

    [PLAYER-2692] Browsebutton updated

--- DiscoverySampleApp/DiscoverySampleApp/Utils/BrowseButton.m
+++ DiscoverySampleApp/DiscoverySampleApp/Utils/BrowseButton.m
@@ -13,7 +13,9 @@
 @synthesize titleLabel;
 
 - (instancetype)initWithFrame:(CGRect)frame {
-  if (self = [super initWithFrame:frame]){
+  if (self = [super initWithFrame:frame]) {
+    self.backgroundColor = UIColor.blackColor;
+
     _transparentView = [[UIView alloc] initWithFrame:frame];
     _transparentView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
     //[self addSubview:self.transparentView];
@@ -21,7 +23,7 @@
     self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 160, 50)];
     self.titleLabel.font = [UIFont fontWithName:@"Roboto-Bold" size:12.0];
     self.titleLabel.textColor = [UIColor whiteColor];
-    
+
     [self addSubview:self.titleLabel];
   }
   return self;
