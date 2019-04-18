dbe6a8d3792adb942b96ffe20aaa138c6f5c828f
Max Kupetskii <maksim_kupetskii@epam.com>
2019-04-18 15:23:47 +0300

    [PLAYER-2692] BrowseButton updated

--- DiscoverySampleApp/DiscoverySampleApp/Utils/BrowseButton.h
+++ DiscoverySampleApp/DiscoverySampleApp/Utils/BrowseButton.h
@@ -12,7 +12,5 @@
 
 @property (nonatomic) NSString *title;
 @property (nonatomic) NSString *embedCode;
-@property (nonatomic) UILabel *titleLabel;
-@property (nonatomic) UIView *transparentView;
 
 @end
--- DiscoverySampleApp/DiscoverySampleApp/Utils/BrowseButton.m
+++ DiscoverySampleApp/DiscoverySampleApp/Utils/BrowseButton.m
@@ -10,21 +10,15 @@
 
 @implementation BrowseButton
 
-@synthesize titleLabel;
-
 - (instancetype)initWithFrame:(CGRect)frame {
   if (self = [super initWithFrame:frame]) {
     self.backgroundColor = UIColor.blackColor;
 
-    _transparentView = [[UIView alloc] initWithFrame:frame];
-    _transparentView.backgroundColor = [UIColor colorWithWhite:0 alpha:0.5];
-    //[self addSubview:self.transparentView];
-    
-    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 160, 50)];
-    self.titleLabel.font = [UIFont fontWithName:@"Roboto-Bold" size:12.0];
-    self.titleLabel.textColor = [UIColor whiteColor];
-
-    [self addSubview:self.titleLabel];
+//    self.titleLabel = [[UILabel alloc] initWithFrame:CGRectMake(10, 70, 160, 50)];
+//    self.titleLabel.font = [UIFont fontWithName:@"Roboto-Bold" size:12.0];
+//    self.titleLabel.textColor = UIColor.whiteColor;
+//
+//    [self addSubview:self.titleLabel];
   }
   return self;
 }
