d472110f2064ae88724cd8dec4b9715c63a17d54
Max Kupetskii <maksim_kupetskii@epam.com>
2019-04-19 21:55:07 +0300

    [PLAYER-5515] tvOS sample app tokens updated

--- PlaybackLab/TVOSSwiftSampleApp/TVOSSwiftSampleApp/Players/OoyalaPlayerTokenPlayerViewController.swift
+++ PlaybackLab/TVOSSwiftSampleApp/TVOSSwiftSampleApp/Players/OoyalaPlayerTokenPlayerViewController.swift
@@ -26,17 +26,19 @@ class OoyalaPlayerTokenPlayerViewController: OOOoyalaTVPlayerViewController, OOE
     player.setEmbedCode(option.embedCode)
     player.play()
   }
-  
-  func token(forEmbedCodes embedCodes: [Any]!, callback: OOEmbedTokenCallback!) {
-    var params: [String: Any] = [:]
+
+  func token(forEmbedCodes embedCodes: [String], callback: @escaping OOEmbedTokenCallback) {
+    let params = ["account_id": accountId]
     
-    params["account_id"] = accountId
-    
-    guard let embedCodes = embedCodes as? [String] else { return }
     let uri = String(format: "/sas/embed_token/%@/%@", option.pcode, embedCodes.joined(separator: ","))
     
     let urlGen = OOEmbeddedSecureURLGenerator(apiKey: apiKey, secret: secret)
-    let embedTokenUrl = urlGen?.secureURL(authorizeHost, uri: uri, params: params)
-    callback(embedTokenUrl?.absoluteString)
+    guard let embedTokenUrl = urlGen.secureURL(forHost: authorizeHost,
+                                               uri: uri,
+                                               params: params) else {
+                                                return callback(nil)
+    }
+
+    callback(embedTokenUrl.absoluteString)
   }
 }
