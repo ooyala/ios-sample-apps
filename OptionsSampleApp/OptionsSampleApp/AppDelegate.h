cd06090bade2478aad2372e67b4f1fe73efb539f
Max Kupetskii <maksim_kupetskii@epam.com>
2019-05-22 09:46:23 +0300

    [PBA-6701] AppDelegate code style applied

--- OptionsSampleApp/OptionsSampleApp/AppDelegate.h
+++ OptionsSampleApp/OptionsSampleApp/AppDelegate.h
@@ -6,14 +6,14 @@
 //  Copyright (c) 2014 ooyala. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+@import UIKit;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
-@property (strong, nonatomic) UIWindow *window;
+@property (nonatomic) UIWindow *window;
 
 //A counter for tracking the number of ooyala sdk events generated across all the test asset plays
-@property (nonatomic,assign) int count;
+@property (nonatomic, assign) int count;
 
 @end
 
